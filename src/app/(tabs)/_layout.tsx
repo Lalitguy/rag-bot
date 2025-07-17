db2daede3ba12f4329f080f37f0254ac6ccbebf0
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-07-18 02:51:10 +0530

    added icons package

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
 
@@ -8,12 +9,26 @@ const TabLayout = () => {
         name="index"
         options={{
           title: "Menu",
+          tabBarIcon: ({ focused }) => (
+            <MaterialIcons
+              name="menu"
+              size={24}
+              color={focused ? "#bbb" : "#eee"}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="second"
         options={{
-          title: "Menu Two",
+          title: "Menu",
+          tabBarIcon: ({ focused }) => (
+            <MaterialIcons
+              name="menu"
+              size={24}
+              color={focused ? "#bbb" : "#eee"}
+            />
+          ),
         }}
       />
     </Tabs>
