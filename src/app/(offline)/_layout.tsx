ede84ed119b3d5d5c18a6e37796cf985ae7fc239
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-08-02 13:38:21 +0530

    offline tab layout change

--- src/app/(offline)/_layout.tsx
+++ src/app/(offline)/_layout.tsx
@@ -1,19 +1,47 @@
+import { COLORS } from "@/src/constants/colors";
 import { MaterialIcons } from "@expo/vector-icons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { Tabs } from "expo-router";
 import React from "react";
 
 const TabLayout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: COLORS.dark,
+          paddingTop: 2,
+          height: 56,
+          borderTopWidth: 0,
+        },
+        tabBarLabelStyle: {
+          fontFamily: "Nunito-Semi",
+          letterSpacing: 0.9,
+          fontSize: 10,
+        },
+        tabBarActiveTintColor: COLORS.button,
+        tabBarInactiveTintColor: COLORS.lightGray,
+        headerStyle: {
+          backgroundColor: COLORS.dark,
+        },
+        headerTitleStyle: {
+          color: COLORS.white,
+          fontFamily: "Nunito-Semi",
+          width: "100%",
+        },
+        tabBarHideOnKeyboard: true,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Menu",
+          title: "Ask the Assistant",
+          tabBarLabel: "Assistant",
           tabBarIcon: ({ focused }) => (
-            <MaterialIcons
-              name="menu"
-              size={24}
-              color={focused ? "#bbb" : "#eee"}
+            <MaterialCommunityIcons
+              name="assistant"
+              size={22}
+              color={focused ? COLORS.button : COLORS.gray}
             />
           ),
         }}
@@ -21,12 +49,13 @@ const TabLayout = () => {
       <Tabs.Screen
         name="second"
         options={{
-          title: "Menu",
+          title: "Add to knowledge base",
+          tabBarLabel: "Resources",
           tabBarIcon: ({ focused }) => (
             <MaterialIcons
-              name="menu"
-              size={24}
-              color={focused ? "#bbb" : "#eee"}
+              name="library-books"
+              size={22}
+              color={focused ? COLORS.button : COLORS.gray}
             />
           ),
         }}
